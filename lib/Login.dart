382e466a066a870a20fc8b0737826e2f758aa372
Maklan Barbosa <makllan@outlook.com>
2023-05-06 11:49:05 -0300

    Second commit

--- lib/Login.dart
+++ lib/Login.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
@@ -11,82 +13,70 @@ class Login extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Araspend'),
-            backgroundColor: Color.fromARGB(255, 246, 155, 50),
-          ),
           body: Column(
-            children: [
-              Expanded(
-                child: SizedBox(height: 1),
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(20, 50, 20, 0),
+            width: double.infinity,
+            child: Text(
+              "Bem vindo !",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Container(
+            width: double.infinity,
+            height: 60,
+            padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+            child: TextField(
+              style: TextStyle(fontSize: 14),
+              decoration: InputDecoration(
+                labelText: 'E-mail',
               ),
-              Center(
-                  child: Container(
-                      padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                      height: 200,
-                      child: Card(
-                          elevation: 10,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(16),
-                          ),
-                          margin: const EdgeInsets.symmetric(
-                              horizontal: 16, vertical: 8),
-                          child: Padding(
-                            padding: const EdgeInsets.all(16),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.stretch,
-                              children: [
-                                Container(
-                                  height: 40,
-                                  child: TextField(
-                                    style: TextStyle(fontSize: 12),
-                                    decoration: InputDecoration(
-                                      labelText: 'E-mail',
-                                      border: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(8),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                const SizedBox(height: 10),
-                                Container(
-                                  height: 40,
-                                  child: TextField(
-                                    style: TextStyle(fontSize: 12),
-                                    obscureText: true,
-                                    decoration: InputDecoration(
-                                      labelText: 'Senha',
-                                      border: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(8),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                const SizedBox(height: 10),
-                                ElevatedButton(
-                                  onPressed: () {},
-                                  child: const Text('Entrar'),
-                                  style: ElevatedButton.styleFrom(
-                                    padding: EdgeInsets.symmetric(
-                                        horizontal: 30, vertical: 10),
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(10),
-                                      side: BorderSide(
-                                        width: 2,
-                                        color: Colors.green,
-                                      ),
-                                    ),
-                                    primary: Color.fromARGB(255, 246, 155, 50),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          )))),
-              Expanded(
-                child: SizedBox(height: 1),
+            ),
+          ),
+          Container(
+            height: 60,
+            padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+            child: TextField(
+              style: TextStyle(fontSize: 14),
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Senha',
               ),
-            ],
-          )),
+            ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Entrar'),
+            style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+                side: BorderSide(
+                  width: 2,
+                  color: Colors.green,
+                ),
+              ),
+              primary: Colors.green,
+            ),
+          ),
+          Expanded(
+            child: SizedBox(height: 1),
+          ),
+          Container(
+            padding: EdgeInsets.only(bottom: 10),
+            child: Text(
+              "Esqueceu sua senha?",
+              style: TextStyle(color: Color.fromARGB(255, 70, 155, 72)),
+            ),
+          )
+        ],
+      )),
     );
   }
 }
